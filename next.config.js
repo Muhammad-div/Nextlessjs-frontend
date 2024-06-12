015b82f1d59a41835670fb0eccff7a177f8a64be
muhammad-div <hashirdeveloper@gmail.com>
2024-06-12 17:39:09 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -5,12 +5,27 @@
 
 module.exports = {
   eslint: {
-    dirs: ['.'],
+    dirs: ["."],
   },
   poweredByHeader: false,
   trailingSlash: true,
   // output: 'export',
-  output: 'export',
+  output: "export",
+  source: "/api/:path*",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    {
+      key: "Access-Control-Allow-Methods",
+      value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    },
+    {
+      key: "Access-Control-Allow-Headers",
+      value:
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    },
+  ],
+
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
